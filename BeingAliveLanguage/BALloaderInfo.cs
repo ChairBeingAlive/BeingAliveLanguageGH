f35e83b21585dd3334d314ed31bd76560b5b3e86
xarthurETH <xliotx@gmail.com>
2022-08-08 17:28:34 +0200

    update, add new data exposure.

--- BeingAliveLanguage/BALloaderInfo.cs
+++ BeingAliveLanguage/BALloaderInfo.cs
@@ -26,7 +26,7 @@ namespace BeingAliveLanguage
         public override string AuthorContact => "ma@arch.ethz.ch";
 
         public override GH_LibraryLicense License => GH_LibraryLicense.opensource;
-        public override string Version => "0.2.1.0";
+        public override string Version => "0.2.1.1";
     }
 
     // update plugin icons in the tab
