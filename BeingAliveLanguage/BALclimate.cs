561c79162f9bfeb05a9a888ddf802bc42d1e8e88
ZMxOmen <xliotx@gmail.com>
2024-01-11 18:38:51 +0100

    Gaussen Comp WIP.

--- BeingAliveLanguage/BALclimate.cs
+++ BeingAliveLanguage/BALclimate.cs
@@ -4,6 +4,11 @@ using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
 
+using Rhino.Geometry;
+using Grasshopper;
+using Grasshopper.Kernel.Data;
+using GH_IO.Types;
+
 namespace BeingAliveLanguage
 {
   public class BALETP : GH_Component
@@ -100,6 +105,112 @@ namespace BeingAliveLanguage
       DA.SetDataList(3, deficit);
       DA.SetDataList(4, reserve);
       DA.SetData(5, maxReserve);
+    }
+  }
+
+  public class BALGaussian : GH_Component
+  {
+    public BALGaussian()
+      : base("Climate_GaussenDiagram", "balClimate_Gaussen",
+          "Automatically draw the Bagnouls-Gaussen diagram with given climate data.",
+          "BAL", "04::climate")
+    {
+    }
+    protected override Bitmap Icon => Properties.Resources.balEvapotranspiration;
+    public override Guid ComponentGuid => new Guid("3C5480D5-32B6-4EAD-A945-4F81D109EBEA");
+
+    protected override void RegisterInputParams(GH_InputParamManager pManager)
+    {
+      pManager.AddPlaneParameter("Plane", "pln", "The plane to draw the diagram.", GH_ParamAccess.item, Plane.WorldXY);
+      pManager[0].Optional = true;
+
+      pManager.AddNumberParameter("Precipitation", "Prec", "Precipitation of given location in 12 months.", GH_ParamAccess.list);
+      pManager[1].Optional = true;
+
+      pManager.AddNumberParameter("Temperature", "Temp", "Temperature of given location in 12 months.", GH_ParamAccess.list);
+      pManager[2].Optional = true;
+
+      pManager.AddNumberParameter("Scale", "s", "Scale of the diagram.", GH_ParamAccess.item, 1.0);
+    }
+
+    protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+    {
+      pManager.AddCurveParameter("Frame", "F", "The Frame of the Gaussen diagram.", GH_ParamAccess.list);
+
+      pManager.AddCurveParameter("TempCrv", "TC", "The temperature curve of the Gaussen diagram.", GH_ParamAccess.list);
+      pManager.AddCurveParameter("PrecCrv", "PC", "The precipitation curve of the Gaussen diagram.", GH_ParamAccess.list);
+
+      pManager.AddPlaneParameter("LabelLocation", "TxtLoc", "The label location of the Gaussen diagram.", GH_ParamAccess.tree);
+      pManager.AddGenericParameter("Label", "Txt", "The label text of the Gaussen diagram.", GH_ParamAccess.tree);
+      //pManager.AddPlaneParameter("testPln", "tP", "xxxx", GH_ParamAccess.item);
+    }
+
+    protected override void SolveInstance(IGH_DataAccess DA)
+    {
+      var pln = Plane.WorldXY;
+      var precLst = new List<double>();
+      var tempLst = new List<double>();
+      double scale = 1.0;
+
+      DA.GetData("Plane", ref pln);
+      DA.GetData("Scale", ref scale);
+
+      // data check
+      if (!DA.GetDataList("Precipitation", precLst) || !DA.GetDataList("Temperature", tempLst))
+      { return; }
+
+      if (precLst.Count != 12 || tempLst.Count != 12)
+      {
+        AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Precipitation and temperature data should be 12 months.");
+        return;
+      }
+
+      // ! frame
+      var horAxis = new Line(pln.Origin, pln.XAxis * 10 * scale);
+      var verAxis1 = new Line(pln.Origin, pln.YAxis * 10 * scale);
+      var verAxis2 = new Line(horAxis.To, pln.YAxis * 10 * scale);
+
+      var monthPtParam = horAxis.ToNurbsCurve().DivideByCount(11, true, out Point3d[] monthPt);
+      var monthAxis = monthPt.ToArray().Select(x => new Line(x, -pln.YAxis * 0.2 * scale)).ToList();
+
+      var frameLn = new List<Line> { horAxis, verAxis1, verAxis2 }.Concat(monthAxis).ToList();
+      DA.SetDataList("Frame", frameLn.Concat(monthAxis));
+
+      // ! curve
+
+
+      // ! label
+      // month label
+      var monLocPt = monthAxis.Select(x => x.To + x.Direction).ToList();
+      var monLoc = monLocPt.Select(x => new Plane(x, pln.XAxis, pln.YAxis)).ToList();
+      var monText = new List<string> { "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" };
+
+      // parcipitation, temp label
+      var parcLocPt = (verAxis1.From + verAxis1.To) * 0.5 - horAxis.Direction * 0.15 * scale;
+      var parcLoc = new Plane(parcLocPt, pln.YAxis, -pln.XAxis);
+      var parcText = "Precipitation (mm)";
+
+      var tempLocPt = (verAxis2.From + verAxis2.To) * 0.5 + horAxis.Direction * 0.15 * scale;
+      var tempLoc = new Plane(tempLocPt, -pln.YAxis, pln.XAxis);
+      var tempText = "Temperature (°C)";
+
+
+      DataTree<Plane> labelLoc = new DataTree<Plane>();
+      labelLoc.AddRange(monLoc, new GH_Path(0));
+      labelLoc.Add(parcLoc, new GH_Path(1));
+      labelLoc.Add(tempLoc, new GH_Path(2));
+
+
+      DataTree<string> labelTxt = new DataTree<string>();
+      labelTxt.AddRange(monText, new GH_Path(0));
+      labelTxt.Add(parcText, new GH_Path(1));
+      labelTxt.Add(tempText, new GH_Path(2));
+
+      DA.SetDataTree(3, labelLoc);
+      DA.SetDataTree(4, labelTxt);
+      //DA.SetData(5, parcLoc);
+
+
     }
   }
 }
