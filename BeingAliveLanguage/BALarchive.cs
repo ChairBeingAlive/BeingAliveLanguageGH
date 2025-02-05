8a5a7035aa28ee431d0ec55acf1060dc4e180727
ZM.ETH.PC <ma@arch.ethz.ch>
2025-02-05 14:05:55 +0100

    bug fix.

--- BeingAliveLanguage/BALarchive.cs
+++ BeingAliveLanguage/BALarchive.cs
@@ -25,6 +25,7 @@ namespace BeingAliveLanguage
     private bool useSI = true; // True for Metric, False for Imperial
     protected override Bitmap Icon => Properties.Resources.balGaussen;
     public override Guid ComponentGuid => new Guid("3C5480D5-32B6-4EAD-A945-4F81D109EBEA");
+    public override GH_Exposure Exposure => GH_Exposure.hidden;
 
     protected override void RegisterInputParams(GH_InputParamManager pManager)
     {
