ebe2d8dbd79b4d039e0c0f7ea29ce336687ee47d
xTree <xliotx@gmail.com>
2025-07-21 09:07:34 +0200

    code formatting.

--- BeingAliveLanguage/BALutils.cs
+++ BeingAliveLanguage/BALutils.cs
@@ -5,94 +5,101 @@ using System.Collections.Generic;
 using System.Linq;
 using BeingAliveLanguage.BalCore;
 
-namespace BeingAliveLanguage
-{
-  public class BALsoilInfo : GH_Component
-  {
-    public BALsoilInfo() :
-        base("Soil_Information", "balSoilInfoText",
-            "Export the soil information in text format.",
-            "BAL", "09::utils")
-    { }
+namespace BeingAliveLanguage {
+  public class BALsoilInfo : GH_Component {
+    public BALsoilInfo()
+        : base("Soil_Information", "balSoilInfoText", "Export the soil information in text format.",
+               "BAL", "09::utils") {}
 
-    public override GH_Exposure Exposure => GH_Exposure.primary; 
-    protected override System.Drawing.Bitmap Icon => SysUtils.cvtByteBitmap(Properties.Resources.balSoilInfo);
+    public override GH_Exposure Exposure => GH_Exposure.primary;
+    protected override System.Drawing.Bitmap Icon =>
+        SysUtils.cvtByteBitmap(Properties.Resources.balSoilInfo);
     public override Guid ComponentGuid => new Guid("af64a14a-6795-469c-b044-7db972d5bd84");
 
-    protected override void RegisterInputParams(GH_InputParamManager pManager)
-    {
-      pManager.AddGenericParameter("Soil Info", "soilInfo", "Info about the current soil based on given content ratio.", GH_ParamAccess.item);
+    protected override void RegisterInputParams(GH_InputParamManager pManager) {
+      pManager.AddGenericParameter("Soil Info", "soilInfo",
+                                   "Info about the current soil based on given content ratio.",
+                                   GH_ParamAccess.item);
     }
 
-    protected override void RegisterOutputParams(GH_OutputParamManager pManager)
-    {
-      pManager.AddTextParameter("Soil Info Text", "soilText", "Soil Info that can be visualized with the TAG component.", GH_ParamAccess.item);
+    protected override void RegisterOutputParams(GH_OutputParamManager pManager) {
+      pManager.AddTextParameter("Soil Info Text", "soilText",
+                                "Soil Info that can be visualized with the TAG component.",
+                                GH_ParamAccess.item);
     }
 
-    protected override void SolveInstance(IGH_DataAccess DA)
-    {
+    protected override void SolveInstance(IGH_DataAccess DA) {
       // get data
       SoilProperty soilInfo = new SoilProperty();
       List<Curve> triCrv = new List<Curve>();
 
-      if (!DA.GetData(0, ref soilInfo))
-      { return; }
-
+      if (!DA.GetData(0, ref soilInfo)) {
+        return;
+      }
 
       var sText = Utils.SoilText(soilInfo);
 
       // assign output
       DA.SetData(0, sText);
     }
-
   }
 
-  public class BALmorphFan : GH_Component
-  {
-    public BALmorphFan() :
-        base("Bound Morph (Fan)", "balMorphFan",
-        "Morph the soil diagram from a rectangle shape into a fan shape.",
-        "BAL", "09::utils")
-    { }
+  public class BALmorphFan : GH_Component {
+    public BALmorphFan()
+        : base("Bound Morph (Fan)", "balMorphFan",
+               "Morph the soil diagram from a rectangle shape into a fan shape.", "BAL",
+               "09::utils") {}
 
     public override GH_Exposure Exposure => GH_Exposure.primary;
-    protected override System.Drawing.Bitmap Icon => SysUtils.cvtByteBitmap(Properties.Resources.balSoilMorphFan);
+    protected override System.Drawing.Bitmap Icon =>
+        SysUtils.cvtByteBitmap(Properties.Resources.balSoilMorphFan);
     public override Guid ComponentGuid => new Guid("e08cd0fa-c27d-4d1b-85fc-da0353bda292");
 
-    protected override void RegisterInputParams(GH_InputParamManager pManager)
-    {
-      pManager.AddGenericParameter("Soil Base", "soilBase", "The base object used for soil diagram generation.", GH_ParamAccess.item);
-      pManager.AddCurveParameter("Soil Geo", "soilG", "Any line- or polyline-based geometries from the soil system.", GH_ParamAccess.list);
+    protected override void RegisterInputParams(GH_InputParamManager pManager) {
+      pManager.AddGenericParameter("Soil Base", "soilBase",
+                                   "The base object used for soil diagram generation.",
+                                   GH_ParamAccess.item);
+      pManager.AddCurveParameter("Soil Geo", "soilG",
+                                 "Any line- or polyline-based geometries from the soil system.",
+                                 GH_ParamAccess.list);
 
-      pManager.AddCurveParameter("Target Fan", "tFan", "The target closed fan shape curve to morph to.", GH_ParamAccess.item);
+      pManager.AddCurveParameter("Target Fan", "tFan",
+                                 "The target closed fan shape curve to morph to.",
+                                 GH_ParamAccess.item);
     }
 
-    protected override void RegisterOutputParams(GH_OutputParamManager pManager)
-    {
-      pManager.AddCurveParameter("Transformed Geo", "fSoilG", "The morphed soil geometries inside the given fan shape.", GH_ParamAccess.list);
-      pManager.AddPlaneParameter("Transformed Plane", "fP", "The based plane for the polar coordinate system used during the mapping process", GH_ParamAccess.item);
+    protected override void RegisterOutputParams(GH_OutputParamManager pManager) {
+      pManager.AddCurveParameter("Transformed Geo", "fSoilG",
+                                 "The morphed soil geometries inside the given fan shape.",
+                                 GH_ParamAccess.list);
+      pManager.AddPlaneParameter(
+          "Transformed Plane", "fP",
+          "The based plane for the polar coordinate system used during the mapping process",
+          GH_ParamAccess.item);
     }
 
-    protected override void SolveInstance(IGH_DataAccess DA)
-    {
+    protected override void SolveInstance(IGH_DataAccess DA) {
       // load input, check validility
       var sBase = new SoilBase();
       var soilG = new List<Curve>();
       Curve tFan = null;
-      if (!DA.GetData("Soil Base", ref sBase))
-      { AddRuntimeMessage(GH_RuntimeMessageLevel.Warning, "no soil base detected, mapping results might be incorrect."); }
+      if (!DA.GetData("Soil Base", ref sBase)) {
+        AddRuntimeMessage(GH_RuntimeMessageLevel.Warning,
+                          "no soil base detected, mapping results might be incorrect.");
+      }
 
-      if (!DA.GetDataList("Soil Geo", soilG))
-      { return; }
+      if (!DA.GetDataList("Soil Geo", soilG)) {
+        return;
+      }
 
-      if (!DA.GetData("Target Fan", ref tFan))
-      { return; }
+      if (!DA.GetData("Target Fan", ref tFan)) {
+        return;
+      }
 
       var polyCrvFan = tFan as PolyCurve;
 
       polyCrvFan.TryGetPlane(out Plane tPln);
-      if (!polyCrvFan.IsPlanar() || tPln == null)
-      {
+      if (!polyCrvFan.IsPlanar() || tPln == null) {
         AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "target boundary is not planar.");
         return;
       }
@@ -102,14 +109,10 @@ namespace BeingAliveLanguage
 
       List<Arc> tArc = new List<Arc>();
       double polarLenRange = 0.0;
-      foreach (var seg in segFan)
-      {
-        if (seg.TryGetArc(out Arc t))
-        {
+      foreach (var seg in segFan) {
+        if (seg.TryGetArc(out Arc t)) {
           tArc.Add(t);
-        }
-        else
-        {
+        } else {
           polarLenRange = seg.GetLength();
         }
       }
@@ -117,8 +120,9 @@ namespace BeingAliveLanguage
       var innerArc = tArc.OrderBy(x => x.Length).First();
 
       // make sure the direction is always CW
-      if (innerArc.Plane.ZAxis * tPln.ZAxis < 0)
-      { innerArc.Reverse(); }
+      if (innerArc.Plane.ZAxis * tPln.ZAxis < 0) {
+        innerArc.Reverse();
+      }
 
       var xDir = innerArc.StartPoint - innerArc.Center;
       var yDir = Vector3d.CrossProduct(tPln.ZAxis, xDir);
@@ -127,7 +131,7 @@ namespace BeingAliveLanguage
       var polarAngRange = Math.Abs(innerArc.EndAngle - innerArc.StartAngle);
       var s0 = innerArc.Radius;
 
-      /* 
+      /*
        * The approach here is to:
        * 1. parametrize the original geometry in the original bounds, origin in the centre
        * 2. convert the parametrized coordinates into the new polar system
@@ -136,13 +140,10 @@ namespace BeingAliveLanguage
        */
 
       var newSoilG = new List<PolylineCurve>();
-      foreach (var crv in soilG)
-      {
-        if (crv.TryGetPolyline(out Polyline polyln))
-        {
+      foreach (var crv in soilG) {
+        if (crv.TryGetPolyline(out Polyline polyln)) {
           var newPoly = new Polyline();
-          foreach (var pt in polyln)
-          {
+          foreach (var pt in polyln) {
             // step 1
             sBase.pln.ClosestParameter(pt, out double s, out double t);
             var sParam = s / sBase.bnd.Width;
@@ -162,23 +163,16 @@ namespace BeingAliveLanguage
           }
 
           newSoilG.Add(newPoly.ToPolylineCurve());
-        }
-        else
-        {
-          AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Soil geometries contain non-polyline-based object(s).");
+        } else {
+          AddRuntimeMessage(GH_RuntimeMessageLevel.Error,
+                            "Soil geometries contain non-polyline-based object(s).");
           return;
         }
       }
 
-
       DA.SetDataList(0, newSoilG);
       DA.SetData(1, basePln);
-
     }
-
   }
 
-
 }
-
-
--- GeoSharPlusNET/NativeBridge.cs
+++ GeoSharPlusNET/NativeBridge.cs
@@ -1,13 +1,9 @@
 using System;
-using System.IO;
-using System.Text;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 
-namespace GSP
-{
-  public static class NativeBridge
-  {
+namespace GSP {
+  public static class NativeBridge {
     private const string WinLibName = @"GeoSharPlusCPP.dll";
     private const string MacLibName = @"libGeoSharPlusCPP.dylib";
 
@@ -29,10 +25,8 @@ namespace GSP
     /// <summary>
     /// Get error messages that can be displayed in Grasshopper components
     /// </summary>
-    public static string[] GetErrorMessages()
-    {
-      lock (_errorLog)
-      {
+    public static string[] GetErrorMessages() {
+      lock (_errorLog) {
         return _errorLog.ToArray();
       }
     }
@@ -40,10 +34,8 @@ namespace GSP
     /// <summary>
     /// Clear the error log
     /// </summary>
-    public static void ClearErrorLog()
-    {
-      lock (_errorLog)
-      {
+    public static void ClearErrorLog() {
+      lock (_errorLog) {
         _errorLog.Clear();
       }
     }
@@ -51,10 +43,8 @@ namespace GSP
     /// <summary>
     /// Add an error message to the log
     /// </summary>
-    private static void LogError(string message)
-    {
-      lock (_errorLog)
-      {
+    private static void LogError(string message) {
+      lock (_errorLog) {
         _errorLog.Add($"[{DateTime.Now:yyyy-MM-dd HH:mm:ss}] {message}");
 
         // Keep log at a reasonable size
@@ -64,29 +54,23 @@ namespace GSP
     }
 
     // Set DllImport search path to include the current assembly directory for macOS
-    static NativeBridge()
-    {
-      if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-      {
-        try
-        {
+    static NativeBridge() {
+      if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX)) {
+        try {
           // List all possible library locations to try
           var searchLocations = new List<string>();
 
           // 1. Load from assembly directory
           string assemblyLocation = System.Reflection.Assembly.GetExecutingAssembly().Location;
           string? assemblyDirectory = Path.GetDirectoryName(assemblyLocation);
-          if (assemblyDirectory != null)
-          {
+          if (assemblyDirectory != null) {
             searchLocations.Add(Path.Combine(assemblyDirectory, MacLibName));
           }
 
           // 2. Try parent directory (sometimes needed for GH plugins)
-          if (assemblyDirectory != null)
-          {
+          if (assemblyDirectory != null) {
             string? parentDir = Path.GetDirectoryName(assemblyDirectory);
-            if (parentDir != null)
-            {
+            if (parentDir != null) {
               searchLocations.Add(Path.Combine(parentDir, MacLibName));
             }
           }
@@ -95,85 +79,66 @@ namespace GSP
           searchLocations.Add(Path.Combine(Directory.GetCurrentDirectory(), MacLibName));
 
           // 4. Add standard system locations
-          searchLocations.Add(MacLibName); // Default system search paths
+          searchLocations.Add(MacLibName);  // Default system search paths
 
           LogError($"Searching for {MacLibName} in the following locations:");
-          foreach (var path in searchLocations)
-          {
+          foreach (var path in searchLocations) {
             LogError($"- {path} (exists: {File.Exists(path)})");
           }
 
           // Try to load from each location
           IntPtr handle = IntPtr.Zero;
-          foreach (var libraryPath in searchLocations)
-          {
-            if (File.Exists(libraryPath))
-            {
+          foreach (var libraryPath in searchLocations) {
+            if (File.Exists(libraryPath)) {
               LogError($"Attempting to load native library from: {libraryPath}");
-              handle = dlopen(libraryPath, 2); // RTLD_NOW = 2
+              handle = dlopen(libraryPath, 2);  // RTLD_NOW = 2
 
-              if (handle != IntPtr.Zero)
-              {
+              if (handle != IntPtr.Zero) {
                 _isNativeLibraryLoaded = true;
                 _loadedLibraryPath = libraryPath;
                 LogError($"Successfully loaded native library from: {libraryPath}");
                 break;
-              }
-              else
-              {
+              } else {
                 string errorMsg = dlerror();
                 LogError($"Failed to load library from {libraryPath}: {errorMsg}");
               }
             }
           }
 
-          if (handle == IntPtr.Zero)
-          {
-            LogError($"Failed to load native library from any location. Calls to native methods will likely fail.");
+          if (handle == IntPtr.Zero) {
+            LogError(
+                $"Failed to load native library from any location. Calls to native methods will likely fail.");
           }
-        }
-        catch (Exception ex)
-        {
+        } catch (Exception ex) {
           LogError($"Exception while setting up native library path: {ex.Message}");
           LogError($"Stack trace: {ex.StackTrace}");
         }
-      }
-      else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-      {
-        try
-        {
+      } else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+        try {
           // Locate the DLL file for Windows
           string assemblyLocation = System.Reflection.Assembly.GetExecutingAssembly().Location;
           string? assemblyDirectory = Path.GetDirectoryName(assemblyLocation);
           string dllPath = string.Empty;
 
-          if (assemblyDirectory != null)
-          {
+          if (assemblyDirectory != null) {
             dllPath = Path.Combine(assemblyDirectory, WinLibName);
-            if (!File.Exists(dllPath))
-            {
+            if (!File.Exists(dllPath)) {
               // Try parent directory
               string? parentDir = Path.GetDirectoryName(assemblyDirectory);
-              if (parentDir != null)
-              {
+              if (parentDir != null) {
                 dllPath = Path.Combine(parentDir, WinLibName);
               }
             }
           }
 
-          if (File.Exists(dllPath))
-          {
+          if (File.Exists(dllPath)) {
             _isNativeLibraryLoaded = true;
             _loadedLibraryPath = dllPath;
             LogError($"Successfully located native library at: {dllPath}");
-          }
-          else
-          {
+          } else {
             LogError($"Failed to locate native library {WinLibName} in expected locations.");
           }
-        }
-        catch (Exception ex)
-        {
+        } catch (Exception ex) {
           LogError($"Exception while locating native library path: {ex.Message}");
           LogError($"Stack trace: {ex.StackTrace}");
         }
@@ -191,22 +156,23 @@ namespace GSP
     // For each function, we create 3 functions: Windows, macOS implementations, and the public API
     // =========
 
-    // Example: Point Round Trip -- Passing a Point3d to C++ and back 
-    [DllImport(WinLibName, EntryPoint = "point3d_roundtrip", CallingConvention = CallingConvention.Cdecl)]
-    private static extern bool Point3dRoundTripWin(byte[] inBuffer, int inSize, out IntPtr outBuffer, out int outSize);
-    [DllImport(MacLibName, EntryPoint = "point3d_roundtrip", CallingConvention = CallingConvention.Cdecl)]
-    private static extern bool Point3dRoundTripMac(byte[] inBuffer, int inSize, out IntPtr outBuffer, out int outSize);
-    public static bool Point3dRoundTrip(byte[] inBuffer, int inSize, out IntPtr outBuffer, out int outSize)
-    {
-      try
-      {
+    // Example: Point Round Trip -- Passing a Point3d to C++ and back
+    [DllImport(WinLibName, EntryPoint = "point3d_roundtrip",
+               CallingConvention = CallingConvention.Cdecl)]
+    private static extern bool Point3dRoundTripWin(byte[] inBuffer, int inSize,
+                                                   out IntPtr outBuffer, out int outSize);
+    [DllImport(MacLibName, EntryPoint = "point3d_roundtrip",
+               CallingConvention = CallingConvention.Cdecl)]
+    private static extern bool Point3dRoundTripMac(byte[] inBuffer, int inSize,
+                                                   out IntPtr outBuffer, out int outSize);
+    public static bool Point3dRoundTrip(byte[] inBuffer, int inSize, out IntPtr outBuffer,
+                                        out int outSize) {
+      try {
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
           return Point3dRoundTripWin(inBuffer, inSize, out outBuffer, out outSize);
         else
           return Point3dRoundTripMac(inBuffer, inSize, out outBuffer, out outSize);
-      }
-      catch (Exception ex)
-      {
+      } catch (Exception ex) {
         LogError($"Exception in Point3dRoundTrip: {ex.Message}");
         outBuffer = IntPtr.Zero;
         outSize = 0;
@@ -215,21 +181,22 @@ namespace GSP
     }
 
     // Example: Point Array Round Trip -- Passing an array of Point3d to C++ and back
-    [DllImport(WinLibName, EntryPoint = "point3d_array_roundtrip", CallingConvention = CallingConvention.Cdecl)]
-    private static extern bool Point3dArrayRoundTripWin(byte[] inBuffer, int inSize, out IntPtr outBuffer, out int outSize);
-    [DllImport(MacLibName, EntryPoint = "point3d_array_roundtrip", CallingConvention = CallingConvention.Cdecl)]
-    private static extern bool Point3dArrayRoundTripMac(byte[] inBuffer, int inSize, out IntPtr outBuffer, out int outSize);
-    public static bool Point3dArrayRoundTrip(byte[] inBuffer, int inSize, out IntPtr outBuffer, out int outSize)
-    {
-      try
-      {
+    [DllImport(WinLibName, EntryPoint = "point3d_array_roundtrip",
+               CallingConvention = CallingConvention.Cdecl)]
+    private static extern bool Point3dArrayRoundTripWin(byte[] inBuffer, int inSize,
+                                                        out IntPtr outBuffer, out int outSize);
+    [DllImport(MacLibName, EntryPoint = "point3d_array_roundtrip",
+               CallingConvention = CallingConvention.Cdecl)]
+    private static extern bool Point3dArrayRoundTripMac(byte[] inBuffer, int inSize,
+                                                        out IntPtr outBuffer, out int outSize);
+    public static bool Point3dArrayRoundTrip(byte[] inBuffer, int inSize, out IntPtr outBuffer,
+                                             out int outSize) {
+      try {
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
           return Point3dArrayRoundTripWin(inBuffer, inSize, out outBuffer, out outSize);
         else
           return Point3dArrayRoundTripMac(inBuffer, inSize, out outBuffer, out outSize);
-      }
-      catch (Exception ex)
-      {
+      } catch (Exception ex) {
         LogError($"Exception in Point3dArrayRoundTrip: {ex.Message}");
         outBuffer = IntPtr.Zero;
         outSize = 0;
@@ -238,21 +205,27 @@ namespace GSP
     }
 
     // BAL Poisson Disk Elimination Sample
-    [DllImport(WinLibName, EntryPoint = "BALpossionDiskElimSample", CallingConvention = CallingConvention.Cdecl)]
-    private static extern bool BALpossionDiskElimSampleWin(byte[] inBuffer, int inSize, double generalArea, int dim, int n, out IntPtr outBuffer, out int outSize);
-    [DllImport(MacLibName, EntryPoint = "BALpossionDiskElimSample", CallingConvention = CallingConvention.Cdecl)]
-    private static extern bool BALpossionDiskElimSampleMac(byte[] inBuffer, int inSize, double generalArea, int dim, int n, out IntPtr outBuffer, out int outSize);
-    public static bool BALpossionDiskElimSample(byte[] inBuffer, int inSize, double generalArea, int dim, int n, out IntPtr outBuffer, out int outSize)
-    {
-      try
-      {
+    [DllImport(WinLibName, EntryPoint = "BALpossionDiskElimSample",
+               CallingConvention = CallingConvention.Cdecl)]
+    private static extern bool BALpossionDiskElimSampleWin(byte[] inBuffer, int inSize,
+                                                           double generalArea, int dim, int n,
+                                                           out IntPtr outBuffer, out int outSize);
+    [DllImport(MacLibName, EntryPoint = "BALpossionDiskElimSample",
+               CallingConvention = CallingConvention.Cdecl)]
+    private static extern bool BALpossionDiskElimSampleMac(byte[] inBuffer, int inSize,
+                                                           double generalArea, int dim, int n,
+                                                           out IntPtr outBuffer, out int outSize);
+    public static bool BALpossionDiskElimSample(byte[] inBuffer, int inSize, double generalArea,
+                                                int dim, int n, out IntPtr outBuffer,
+                                                out int outSize) {
+      try {
         if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-          return BALpossionDiskElimSampleWin(inBuffer, inSize, generalArea, dim, n, out outBuffer, out outSize);
+          return BALpossionDiskElimSampleWin(inBuffer, inSize, generalArea, dim, n, out outBuffer,
+                                             out outSize);
         else
-          return BALpossionDiskElimSampleMac(inBuffer, inSize, generalArea, dim, n, out outBuffer, out outSize);
-      }
-      catch (Exception ex)
-      {
+          return BALpossionDiskElimSampleMac(inBuffer, inSize, generalArea, dim, n, out outBuffer,
+                                             out outSize);
+      } catch (Exception ex) {
         LogError($"Exception in BALpossionDiskElimSample: {ex.Message}");
         outBuffer = IntPtr.Zero;
         outSize = 0;
