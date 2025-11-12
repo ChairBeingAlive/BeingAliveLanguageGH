8e06b09bd478b9ab8a62586776fc3b242f50cf1f
xTree <xliotx@gmail.com>
2025-11-12 11:02:47 +0100

    format.

--- BeingAliveLanguage/BALMetaInfo.cs
+++ BeingAliveLanguage/BALMetaInfo.cs
@@ -1,63 +1,51 @@
 using System;
 using System.IO;
-using System.Reflection;
 using Grasshopper.Kernel;
 
-namespace BeingAliveLanguage
-{
-  public class BALinfoExporter : GH_Component
-  {
-    public BALinfoExporter() :
-      base("BAL_Info", "balInfo",
-          "Check BAL version and whether everything is loaded correctly.",
-          "BAL", "00::info")
-    { }
+namespace BeingAliveLanguage {
+public class BALinfoExporter : GH_Component {
+  public BALinfoExporter()
+      : base("BAL_Info",
+             "balInfo",
+             "Check BAL version and whether everything is loaded correctly.",
+             "BAL",
+             "00::info") {}
 
-    public override GH_Exposure Exposure => GH_Exposure.primary;
-    //protected override System.Drawing.Bitmap Icon => null;
-    public override Guid ComponentGuid => new Guid("dc1c6827-4e84-48f5-be11-a18e9f410291");
+  public override GH_Exposure Exposure => GH_Exposure.primary;
+  // protected override System.Drawing.Bitmap Icon => null;
+  public override Guid ComponentGuid => new Guid("dc1c6827-4e84-48f5-be11-a18e9f410291");
 
-    protected override void RegisterInputParams(GH_InputParamManager pManager)
-    {
-    }
+  protected override void RegisterInputParams(GH_InputParamManager pManager) {}
 
-    protected override void RegisterOutputParams(GH_OutputParamManager pManager)
-    {
-      pManager.AddTextParameter("Info", "info", "Plugin info for loading native lib.", GH_ParamAccess.item);
-    }
+  protected override void RegisterOutputParams(GH_OutputParamManager pManager) {
+    pManager.AddTextParameter(
+        "Info", "info", "Plugin info for loading native lib.", GH_ParamAccess.item);
+  }
 
-    protected override void SolveInstance(IGH_DataAccess DA)
-    {
-      try
-      {
-        // Check if the native library is loaded before proceeding
-        if (!GSP.NativeBridge.IsNativeLibraryLoaded)
-        {
-          string[] errors = GSP.NativeBridge.GetErrorMessages();
-          string errorMsg = string.Join("\n", errors);
-          AddRuntimeMessage(GH_RuntimeMessageLevel.Error,
-              $"Native library not loaded. Check the following errors:\n{errorMsg}");
-          return;
-        }
-        else
-        {
-          // Create an instance of BeingAliveLanguageInfo to access the non-static property
-          string version = new BeingAliveLanguageInfo().AssemblyVersion;
-          string loadedPath = GSP.NativeBridge.LoadedLibraryPath;
-          string libName = Path.GetFileName(loadedPath);
+  protected override void SolveInstance(IGH_DataAccess DA) {
+    try {
+      // Check if the native library is loaded before proceeding
+      if (!GSP.NativeBridge.IsNativeLibraryLoaded) {
+        string[] errors = GSP.NativeBridge.GetErrorMessages();
+        string errorMsg = string.Join("\n", errors);
+        AddRuntimeMessage(GH_RuntimeMessageLevel.Error,
+                          $"Native library not loaded. Check the following errors:\n{errorMsg}");
+        return;
+      } else {
+        // Create an instance of BeingAliveLanguageInfo to access the non-static property
+        string version = new BeingAliveLanguageInfo().AssemblyVersion;
+        string loadedPath = GSP.NativeBridge.LoadedLibraryPath;
+        string libName = Path.GetFileName(loadedPath);
 
-          string infoText = $"Native library loaded successfully from: {loadedPath}\n" +
-                            $"Library name: {libName}\n" +
-                            $"Version: {version}";
+        string infoText = $"Native library loaded successfully from: {loadedPath}\n" +
+                          $"Library name: {libName}\n" + $"Version: {version}";
 
-          DA.SetData(0, infoText);
-        }
-
+        DA.SetData(0, infoText);
       }
-      catch (Exception ex)
-      {
-        AddRuntimeMessage(GH_RuntimeMessageLevel.Error, $"Exception: {ex.Message}");
-      }
+
+    } catch (Exception ex) {
+      AddRuntimeMessage(GH_RuntimeMessageLevel.Error, $"Exception: {ex.Message}");
     }
   }
+}
 }
